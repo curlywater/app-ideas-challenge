911f8b036c86b051b18c03426f6948be80bdff40
curly <water.curly@gmail.com>
2020-09-03 17:23:05 +0800

    build(webpack): 修复编译错误问题

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,16 @@
 const path = require("path");
+const {whenProd} = require("@craco/craco");
 module.exports = {
     webpack: {
         alias: {
             "src": path.resolve(__dirname, "./src")
         },
-        output: {
-            publicPath: "/app-ideas-challenge/"
+        configure: {
+            ...whenProd(() => ({
+                output: {
+                    publicPath: "/app-ideas-challenge/"
+                }
+            }), {})
         }
     }
 }
